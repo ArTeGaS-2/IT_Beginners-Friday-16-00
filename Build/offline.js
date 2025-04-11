e78a7a9378981b5380f82dc29ca14b391bfd21e7
ArTeGaS-2 <probablytorvik@gmail.com>
2025-04-11 19:58:52 +0300

    HTML Build та завантаження на itch.io

---
+++ Build/offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1744381771,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/player-sheet2.png",
+		"images/collectible-sheet0.png",
+		"images/collectible_2-sheet0.png",
+		"images/danger-sheet0.png",
+		"images/obstacle-sheet0.png",
+		"images/obstacle_2-sheet0.png",
+		"images/targetenemy-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
